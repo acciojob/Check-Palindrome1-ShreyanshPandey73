28de398deaba9c24f9befdd71df5de2bdd0a7943
ShreyanshPandey73 <117717418+ShreyanshPandey73@users.noreply.github.com>
2023-03-16 21:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // complete the given function
 
-function palindrome(str){
-	let j = str.length -1;
-      for( let i = 0 ; i < j/2 ;i++)
-      {
-        let x = str[i] ;//forward character
-        let y = str[j-i];//backward character
-        if( x != y)
-        {
-          // return false if string not match
-          return false;
-        }
-      }
-      /// return true if string is palindrome
-      return true;
+function palindrome(string){
+	 // convert string to an array
+    const arrayValues = string.split('');
+
+    // reverse the array values
+    const reverseArrayValues = arrayValues.reverse();
+
+    // convert array to string
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 module.exports = palindrome
